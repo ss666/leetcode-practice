beb372d7b301c20f384e3e827bc032eb541065ed
ss666 <390105636@qq.com>
2020-05-07 14:55:14 +0100

    .

--- PalindromeNumber.py
+++ PalindromeNumber.py
@@ -1,1 +1,14 @@
- 
+class Solution:
+    def isPalindrome(self, x:int) -> bool:
+        if(x<0):
+            return False
+        return str(x) == str(x)[::-1]
+                       # ''.join(reversed(str(x)))
+                        '''
+                         1.slice() parameters: start/stop/step
+                         2.reversed() function: returns an iterator that
+                        accesses the given sequence in the reverse order.
+                        We have to convert the iterators to list or merge
+                        all characters resulting from the reversed iteration
+                        into a new string.
+                        '''
